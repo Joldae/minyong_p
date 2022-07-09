c310672c0be1f3965643d9a943da0eaed35467e7
Joldae <76109507+Joldae@users.noreply.github.com>
2022-07-09 17:34:14 +0900

    7.9 commit
    스크롤값 받아서 nav_bar 색상 변경 구현 완료

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const content = "가나다라마바사";
 const text = document.querySelector(".text");
 const nav_bar = document.querySelector(".nav_bar");
-
+const main_box = document.querySelector(".main_box");
 // 글씨 타이핑효과
 let i = 0;
 function typing() {
@@ -18,8 +18,13 @@ setInterval(typing, 800);
 
 // 스크롤시 nav 색 변하기
 
-window.addEventListener("scroll", nav_change);
+window.addEventListener("scroll", scrollReturn);
 
-function nav_change() {
-  nav_bar.style.backgroundColor = "blue";
+function scrollReturn() {
+  let scrollY = this.scrollY;
+  if (scrollY > 50) {
+    nav_bar.classList.add("scroll_down");
+  } else {
+    nav_bar.classList.remove("scroll_down");
+  }
 }
